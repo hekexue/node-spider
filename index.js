35c0ea9b17cb3015ac890b160fa5150918f932f8
David Oliveros <dato.oliveros@gmail.com>
2014-08-23 19:31:03 -0700

    Added tests, refactored some code

--- index.js
+++ index.js
@@ -14,11 +14,10 @@
 ( function() {
 	'use strict';
 	
-	var querystring = require('querystring'),
-		debug = require('debug')('spider'),
+	var debug = require('debug')('spider'),
 
+		request = require('request').defaults({ jar: true }),
 		cheerio = require('cheerio'),
-		request = require('request'),
 		events = require('events'),
 		util = require('util'),
 		_ = require('lodash'),
@@ -27,79 +26,113 @@
 	function Spider(options) {
 		var spider = this;
 
+		if( _.isFunction(options) )
+			options = { scraper: options };
+
 		if ( !_.isObject(options) )
-			throw new Error('Options is missing.');
+			return console.error('Spider: arguments are missing');
 		
+		this.status      = 'starting';
 		this.counts      = { retries: 0, pages: 0, items: 0 };
 		this.maxRetries  = options.maxRetries || 100;
-		this.alive       = false;
+
+		this.proxy       = options.proxy || null;
+		this.request     = options.request || request;
 		
+		this.scraper     = options.scraper.bind(spider);
+
 		this.headers = _.defaults(options.headers || {}, {
 			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
 		});
 
-		this.urlTemplate = options.urlTemplate;
-		this.scraper     = options.scraper;
-		this.proxy       = options.proxy || null;
+		this.initial    = options.initial || {};
 
-		this.defaults    = _.extend(options.defaults || {}, {
-			start: 0,
-			windowSize: 100,
-		});
+		this.urlTemplate = options.urlTemplate;
 
 		if ( !this.urlTemplate ) {
 			this.urlTemplate = function(params) {
-				return params.query 
+				return params.query;
 			};
+
 			this.urlTemplate.isStatic = true;
+		};
+
+		// Auto-start if target is set
+		if ( options.target ) {
+			this.query( options.target );
 		}
+
+		Object.defineProperty(this, 'target', {
+			get: function() {
+				return this._target;
+			},
+			set: function(newTarget) {
+				this._target = newTarget;
+				spider.emit('target', newTarget);
+			},
+		});
+
+		this.alive = true;
 	};
 
 	// Prototype inheritance - EventEmitter
 	//
-	Spider.prototype = Object.create( events.EventEmitter.prototype );
-
-	// Start the spider
-	Spider.prototype.start = function startSpider(target) {
-		debug('Starting using scraper: ' + this.scraper.name);
-
-		this.alive = true;
-		this.emit('start', target, this);
-
-		this.move( _.defaults(target, this.defaults) );
-	};
+	util.inherits(Spider, events.EventEmitter);
 
 	// Move the spider to an address
-	Spider.prototype.move = function moveSpider(target) {
-		if ( !this.alive ) return;
+	Spider.prototype.query = function moveSpider(target) {
 		var spider = this;
 
-		// Build the URL
-		var query = {
-			url: spider.urlTemplate(target),
-			proxy: spider.proxy,
-			headers: spider.headers,
-		};
+		if ( !spider.alive ) return;
 
-		// Do an http request
+		
+		if ( !spider.target && !target )
+			return spider.emit('error', 'No target specified.');
+
+		if ( _.isString(target) )
+			target = { query: target };
+
+		spider.target = target;
+		var query = spider.buildQuery();
+
+		// Do the http request
 		debug('Moving '+query.url);
 		spider.emit('move', query);
 
-		request(query, function parseHtml(err, res, body) { 
-			spider.parseHtml(err, res, body, target); 
+		spider.request(query, function(err, res, body) {
+			spider.parseHtml(err, res, body);
 		});
+
+		return spider;
+	};
+
+	Spider.prototype.buildQuery = function queryBuilder() {
+		var query = {};
+
+		if ( this.urlTemplate.isStatic )
+			query.url = this.target.query;
+
+		else
+			query.url = this.urlTemplate( _.defaults(this.target, this.initial) );
+
+		query.proxy = this.proxy;
+		query.headers = this.headers;
+
+		return query;
 	};
 
 	// Parse the response of an HTTP request 
-	Spider.prototype.parseHtml = function htmlParser(err, res, body, target) {
+	Spider.prototype.parseHtml = function htmlParser(err, res, body) {
 		var spider = this,
+			target = spider.target,
 			counts = spider.counts;
 
 		// On Success
-		if (!err && 200 >= res.statusCode && res.statusCode < 400) {
+		// Dummy using true
+		if (true || !err && 200 >= res.statusCode && res.statusCode < 400) {
 			debug('Got OK :)');
 
-			var data = spider.scraper(body);
+			var data = spider.scraper( cheerio.load(body) );
 
 			counts.items += data.items.length;
 			data.page    =  ++counts.pages;
@@ -108,11 +141,15 @@
 			spider.emit('data', data);
 
 			if ( data.more && !spider.urlTemplate.isStatic ) {
-				target.start += target.windowSize;
+
+				target = _.extend(target, {
+					start: target.windowSize + (target.windowSize || 0),
+					page: target.page + 1,
+				});
 
 				spider.moveTimeout = setTimeout( function() {
 					spider.moveTimeout = null;
-					spider.move(target);
+					spider.query(target);
 				}, spider.nextPageDelay);
 
 			} else
@@ -129,7 +166,10 @@
 			//
 			if ( spider.counts.retries < spider.maxRetries ) {
 				debug('Our IP was blocked. (Retry '+spider.counts.retries+')');
-				spider.emit('ipBlocked', target, message, spider);
+				
+				// Pause the spider
+				spider.paused = true;
+				spider.emit('ipBlocked', target, message, body);
 			}
 
 			else {
@@ -140,7 +180,12 @@
 
 	// Stops the spider
 	Spider.prototype.kill = function stopSpider(data) {
-		_.defaults(data, { message: 'Stopping spider', code: 500 });
+		data = data || {};
+
+		_.defaults(data, { 
+			message: 'Stopping spider', 
+			code: 200 
+		});
 
 		clearTimeout(this.moveTimeout);
 
